55dd13ff46a1315aa03868750c8ddd910835b473
Enzoskr <escuderoenzochechu@gmail.com>
2023-06-12 13:43:27 -0300

    adds

---
+++ data.js
@@ -0,0 +1,37 @@
+// const datapagina= [
+//     {
+//         id: 1,
+//         name:"Enz store"
+//         typepage:"landing page"
+//         typelang:
+//         pageimg:
+//     }
+
+//     {
+//         id: 1,
+//         name:
+//         typepage:
+//         typelang:
+//     }
+
+//     {
+//         id: 1,
+//         name:
+//         typepage:
+//         typelang:
+//     }
+
+//     {
+//         id: 1,
+//         name:
+//         typepage:
+//         typelang:
+//     }
+
+//     {
+//         id: 1,
+//         name:
+//         typepage:
+//         typelang:
+//     }
+// ]
